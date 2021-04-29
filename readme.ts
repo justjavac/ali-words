5875c426d8688a422d96991abd70db78cd088eb4
justjavac <justjavac@gmail.com>
2021-04-29 22:30:22 +0800

    添加修改说明

--- readme.ts
+++ readme.ts
@@ -5,6 +5,16 @@ const readme = `# 互联网黑话词汇表
 
 互联网黑话词汇表，包含“赋能、抓手、闭环、沉淀、打通”等阿里味儿词汇。
 
+## 概述
+
+本文词汇收集自网络，如果有遗漏，请修改 [mod.ts](./mod.ts) 文件，然后运行以下命令：
+
+\`\`\`bash
+$ deno run --allow-write .\readme.ts
+\`\`\`
+
+如果你没有安装 Deno，需要先[安装 Deno](https://x.deno.js.cn) 才能运行。
+
 ## 二字词汇
 
 ${words.filter((x) => x.length === 2).join(", ")}
