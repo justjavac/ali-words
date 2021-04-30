b370c7166d0a9fc46ff9d6c41fad77d50682cf03
chenfu <chenfu@tsign.cn>
2021-04-30 17:45:09 +0800

    feat: 补完全部词汇

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import { randomGetWord, getNouns, getVerbs, randomQZoneSentense } from './src/mod.ts';
 
-console.info('verbs:', getVerbs());
-console.info('nouns:', getNouns());
+// console.info('verbs:', getVerbs());
+// console.info('nouns:', getNouns());
 console.info('random one:', randomGetWord());
 
 const qzoneSentence = randomQZoneSentense()
